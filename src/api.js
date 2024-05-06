724e6fddbdae0d338d6f3bc835ca4a653c3bd06e
Nico <akebra77@gmail.com>
2024-05-06 14:47:04 +0200

    Set backend host

--- src/api.js
+++ src/api.js
@@ -1,7 +1,14 @@
+// export function apiPath(route) {
+//     const host = process.env.API_HOST || 'http://localhost';
+//     const port = process.env.API_PORT || 4051;
+//     const basePath = process.env.BASE_PATH || '/api';
+  
+//     return `${host}:${port}${basePath}${route}`; 
+// }
+
 export function apiPath(route) {
-    const host = process.env.API_HOST || 'http://localhost';
-    const port = process.env.API_PORT || 4051;
+    const host = 'https://time-off-nu.vercel.app/';
     const basePath = process.env.BASE_PATH || '/api';
   
-    return `${host}:${port}${basePath}${route}`; 
+    return `${host}${basePath}${route}`; 
 }
