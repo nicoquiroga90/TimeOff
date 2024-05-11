ca448bc9fce3d2a5d205752b3c03a89df53f19cd
Nico <akebra77@gmail.com>
2024-05-11 13:23:29 +0200

    Fixing routering bug

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,23 +1,28 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
 const teamsRouter = require("./API/teams");
 const membersRouter = require("./API/members");
 const timeoffRouter = require("./API/timeoff");
-const buildPath = path.join(__dirname, "../../dist");
 const cors = require("cors");
-
-app.use(express.static(buildPath));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(cors());
 
 app.use("/api/teams", teamsRouter);
 app.use("/api/members", membersRouter);
 app.use("/api/timeoff", timeoffRouter);
 
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const buildPath = path.join(__dirname, "../client/build");
+
+app.use(express.static(buildPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
 
 module.exports = app;
